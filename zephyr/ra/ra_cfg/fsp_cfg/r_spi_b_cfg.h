e26a93509c881c9d19d917ba9cdadcc0b08f8f79
Duy Nguyen <duy.nguyen.xa@renesas.com>
2024-04-11 01:10:19 +0000

    hal: renesas: ra: Update SPI_B hal support for RA
    Update driver code for SPI_B to support for RA8 MCU
    family SPI interface on Zephyr
    
    Signed-off-by: Duy Nguyen <duy.nguyen.xa@renesas.com>

---
+++ zephyr/ra/ra_cfg/fsp_cfg/r_spi_b_cfg.h
@@ -0,0 +1,24 @@
+/*
+* Copyright (c) 2020 - 2024 Renesas Electronics Corporation and/or its affiliates
+*
+* SPDX-License-Identifier: BSD-3-Clause
+*/
+
+#ifndef R_SPI_B_CFG_H_
+#define R_SPI_B_CFG_H_
+#ifdef __cplusplus
+        extern "C" {
+        #endif
+
+#define SPI_B_CFG_PARAM_CHECKING_ENABLE (1)
+#if CONFIG_SPI_B_RA_DTC
+#define SPI_B_DTC_SUPPORT_ENABLE (1)
+#else
+#define SPI_B_DTC_SUPPORT_ENABLE (0)
+#endif
+#define SPI_B_TRANSMIT_FROM_RXI_ISR (0)
+
+#ifdef __cplusplus
+        }
+        #endif
+#endif /* R_SPI_B_CFG_H_ */
