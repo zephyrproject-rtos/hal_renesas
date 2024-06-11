6ebe2c40fd7c658db41f20da9f3f5e89230ae3fa
TriNguyen <tri.nguyen.wj@bp.renesas.com>
2024-06-11 09:00:51 +0700

    hal: renesas: ra: Initial support for SPI
    Initial HAL layer support for SPI driver
    
    Signed-off-by: TriNguyen <tri.nguyen.wj@bp.renesas.com>
    Signed-off-by: Thao Luong <thao.luong.uw@renesas.com>

---
+++ zephyr/ra/ra_cfg/fsp_cfg/r_spi_cfg.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2020 - 2024 Renesas Electronics Corporation and/or its
+ * affiliates
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef R_SPI_CFG_H_
+#define R_SPI_CFG_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define SPI_CFG_PARAM_CHECKING_ENABLE (1)
+#if CONFIG_SPI_RA_DTC
+#define SPI_DMA_SUPPORT_ENABLE (1)
+#else
+#define SPI_DMA_SUPPORT_ENABLE (0)
+#endif
+#define SPI_TRANSMIT_FROM_RXI_ISR (0)
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* R_SPI_CFG_H_ */
