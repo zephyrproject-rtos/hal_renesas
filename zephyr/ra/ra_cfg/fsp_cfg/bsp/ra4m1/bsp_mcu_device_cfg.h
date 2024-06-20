8e10eab298d773fc9e40aedcb8412dc52b3ad6b5
TriNguyen <tri.nguyen.wj@bp.renesas.com>
2024-06-20 15:14:21 +0700

    hal: renesas: Add BSP config RA4M1
    Initial support bsp config for RA4M1 board
    
    Signed-off-by: TriNguyen <tri.nguyen.wj@bp.renesas.com>

---
+++ zephyr/ra/ra_cfg/fsp_cfg/bsp/ra4m1/bsp_mcu_device_cfg.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2020 - 2024 Renesas Electronics Corporation and/or its affiliates
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef BSP_MCU_DEVICE_CFG_H_
+#define BSP_MCU_DEVICE_CFG_H_
+#define BSP_CFG_MCU_PART_SERIES (4)
+#endif /* BSP_MCU_DEVICE_CFG_H_ */
