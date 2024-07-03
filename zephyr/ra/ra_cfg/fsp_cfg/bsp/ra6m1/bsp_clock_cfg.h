1af756387aa235c2df63034b0c3564ce99389068
Quy Tran <quy.tran.pz@renesas.com>
2024-07-03 07:10:23 +0000

    hal: renesas: Update pll multiple calculation
    Update PLL multiple calculation value
    
    Signed-off-by: Quy Tran <quy.tran.pz@renesas.com>

--- zephyr/ra/ra_cfg/fsp_cfg/bsp/ra6m1/bsp_clock_cfg.h
+++ zephyr/ra/ra_cfg/fsp_cfg/bsp/ra6m1/bsp_clock_cfg.h
@@ -31,11 +31,13 @@
 #define BSP_CFG_PLL_SOURCE                                                                         \
 	BSP_CLOCK_PROP_HAS_STATUS_OKAY_OR(DT_NODELABEL(pll), source, RA_PLL_SOURCE_DISABLE)
 #define BSP_CFG_PLL_DIV BSP_CLOCK_PROP_HAS_STATUS_OKAY_OR(DT_NODELABEL(pll), div, RA_PLL_DIV_1)
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(pll), okay)
 #define BSP_CFG_PLL_MUL                                                                            \
-	DT_NODE_HAS_STATUS(DT_NODELABEL(pll), okay)                                                \
-	? BSP_CLOCKS_PLL_MUL(DT_PROP_BY_IDX(DT_NODELABEL(pll), mul, 0),                            \
-			     DT_PROP_BY_IDX(DT_NODELABEL(pll), mul, 1))                            \
-	: BSP_CLOCKS_PLL_MUL(0, 0)
+	BSP_CLOCKS_PLL_MUL(DT_PROP_BY_IDX(DT_NODELABEL(pll), mul, 0),                            \
+			DT_PROP_BY_IDX(DT_NODELABEL(pll), mul, 1))
+#else
+#define BSP_CFG_PLL_MULv BSP_CLOCKS_PLL_MUL(0, 0)
+#endif
 
 #define BSP_CFG_CLOCK_SOURCE                                                                       \
 	BSP_CLOCK_PROP_HAS_STATUS_OKAY_OR(DT_NODELABEL(pclkblock), sysclock_src,                  \
