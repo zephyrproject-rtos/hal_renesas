633ed409283b1634b8b7bcb5724284d945536980
Duy Phuong Hoang. Nguyen <duy.nguyen.xa@renesas.com>
2024-07-03 13:38:03 +0700

    hal: ra: Fix define config for PLL mul macro
    The previous define is complicated and missing parenthesis
    which cause wrong calculation of PLL frequency store in the
    clock array
    This update change the PLL config define to use preprocessor
    to define value from device tree or default value when PLL is
    disable
    
    Signed-off-by: Duy Phuong Hoang. Nguyen <duy.nguyen.xa@renesas.com>

--- zephyr/ra/ra_cfg/fsp_cfg/bsp/ra8t1/bsp_clock_cfg.h
+++ zephyr/ra/ra_cfg/fsp_cfg/bsp/ra8t1/bsp_clock_cfg.h
@@ -33,11 +33,14 @@
 #define BSP_CFG_PLL_SOURCE                                                                         \
 	BSP_CLOCK_PROP_HAS_STATUS_OKAY_OR(DT_NODELABEL(pll), source, RA_PLL_SOURCE_DISABLE)
 #define BSP_CFG_PLL_DIV BSP_CLOCK_PROP_HAS_STATUS_OKAY_OR(DT_NODELABEL(pll), div, RA_PLL_DIV_1)
+
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(pll), okay)
 #define BSP_CFG_PLL_MUL                                                                            \
-	DT_NODE_HAS_STATUS(DT_NODELABEL(pll), okay)                                                \
-	? BSP_CLOCKS_PLL_MUL(DT_PROP_BY_IDX(DT_NODELABEL(pll), mul, 0),                            \
-			     DT_PROP_BY_IDX(DT_NODELABEL(pll), mul, 1))                            \
-	: BSP_CLOCKS_PLL_MUL(0, 0)
+	BSP_CLOCKS_PLL_MUL(DT_PROP_BY_IDX(DT_NODELABEL(pll), mul, 0),                              \
+			   DT_PROP_BY_IDX(DT_NODELABEL(pll), mul, 1))
+#else
+#define BSP_CFG_PLL_MUL BSP_CLOCKS_PLL_MUL(0, 0)
+#endif
 
 #define BSP_CFG_PLODIVP            BSP_CLOCK_PROP_HAS_STATUS_OKAY_OR(DT_NODELABEL(pll), divp, RA_PLL_DIV_2)
 #define BSP_CFG_PLL1P_FREQUENCY_HZ BSP_CLOCK_PROP_HAS_STATUS_OKAY_OR(DT_NODELABEL(pll), freqp, 0)
@@ -49,11 +52,14 @@
 #define BSP_CFG_PLL2_SOURCE                                                                        \
 	BSP_CLOCK_PROP_HAS_STATUS_OKAY_OR(DT_NODELABEL(pll2), source, RA_PLL_SOURCE_DISABLE)
 #define BSP_CFG_PLL2_DIV BSP_CLOCK_PROP_HAS_STATUS_OKAY_OR(DT_NODELABEL(pll2), div, RA_PLL_DIV_1)
-#define BSP_CFG_PLL2_MUL                                                                           \
-	DT_NODE_HAS_STATUS(DT_NODELABEL(pll2), okay)                                               \
-	? BSP_CLOCKS_PLL_MUL(DT_PROP_BY_IDX(DT_NODELABEL(pll2), mul, 0),                           \
-			     DT_PROP_BY_IDX(DT_NODELABEL(pll2), mul, 1))                           \
-	: BSP_CLOCKS_PLL_MUL(0, 0)
+
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(pll), okay)
+#define BSP_CFG_PLL_MUL                                                                            \
+	BSP_CLOCKS_PLL_MUL(DT_PROP_BY_IDX(DT_NODELABEL(pll), mul, 0),                              \
+			   DT_PROP_BY_IDX(DT_NODELABEL(pll), mul, 1))
+#else
+#define BSP_CFG_PLL_MUL BSP_CLOCKS_PLL_MUL(0, 0)
+#endif
 
 #define BSP_CFG_PL2ODIVP           BSP_CLOCK_PROP_HAS_STATUS_OKAY_OR(DT_NODELABEL(pll2), divp, RA_PLL_DIV_2)
 #define BSP_CFG_PLL2P_FREQUENCY_HZ BSP_CLOCK_PROP_HAS_STATUS_OKAY_OR(DT_NODELABEL(pll2), freqp, 0)
