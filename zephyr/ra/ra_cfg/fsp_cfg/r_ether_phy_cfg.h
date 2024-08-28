dc1be985f18e5f5c592793e2716b842f43b775de
Thao Luong <thao.luong.uw@renesas.com>
2024-08-29 00:19:05 +0700

    hal: ra: Update Ethernet driver for RA board
    Initial support for Ethernet and Ethernet phy driver
    
    Signed-off-by: Duy Phuong Hoang. Nguyen <duy.nguyen.xa@renesas.com>
    Signed-off-by: Thao Luong <thao.luong.uw@renesas.com>

---
+++ zephyr/ra/ra_cfg/fsp_cfg/r_ether_phy_cfg.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2020 - 2024 Renesas Electronics Corporation and/or its
+ * affiliates
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#ifndef R_ETHER_PHY_CFG_H_
+#define R_ETHER_PHY_CFG_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if DT_PROP(DT_NODELABEL(eth), phy_type) == 4
+#define ETHER_PHY_CFG_TARGET_ICS1894_ENABLE 1
+#elif DT_PROP(DT_NODELABEL(eth), phy_type) == 3
+#define ETHER_PHY_CFG_TARGET_DP83620_ENABLE 1
+#elif DT_PROP(DT_NODELABEL(eth), phy_type) == 2
+#define ETHER_PHY_CFG_TARGET_KSZ8041_ENABLE 1
+#elif DT_PROP(DT_NODELABEL(eth), phy_type) == 1
+#define ETHER_PHY_CFG_TARGET_KSZ8091RNB_ENABLE 1
+#else
+#error "Invalid LSI type"
+#endif
+
+#define ETHER_PHY_LSI_TYPE_KIT_COMPONENT DT_PROP(DT_NODELABEL(eth), phy_type)
+#define BOARD_PHY_REF_CLK (1)
+
+#define ETHER_PHY_CFG_PARAM_CHECKING_ENABLE (BSP_CFG_PARAM_CHECKING_ENABLE)
+#ifndef ETHER_PHY_CFG_TARGET_KSZ8091RNB_ENABLE
+#define ETHER_PHY_CFG_TARGET_KSZ8091RNB_ENABLE (0)
+#endif
+#ifndef ETHER_PHY_CFG_TARGET_KSZ8041_ENABLE
+#define ETHER_PHY_CFG_TARGET_KSZ8041_ENABLE (1)
+#endif
+#ifndef ETHER_PHY_CFG_TARGET_DP83620_ENABLE
+#define ETHER_PHY_CFG_TARGET_DP83620_ENABLE (0)
+#endif
+#ifndef ETHER_PHY_CFG_TARGET_ICS1894_ENABLE
+#define ETHER_PHY_CFG_TARGET_ICS1894_ENABLE (0)
+#endif
+#ifndef ETHER_PHY_CFG_USE_CUSTOM_PHY_LSI_ENABLE
+#define ETHER_PHY_CFG_USE_CUSTOM_PHY_LSI_ENABLE (0)
+#endif
+#define ETHER_PHY_CFG_USE_REF_CLK (BOARD_PHY_REF_CLK)
+#define ETHER_PHY_CFG_INIT_PHY_LSI_AUTOMATIC (1)
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* R_ETHER_PHY_CFG_H_ */
