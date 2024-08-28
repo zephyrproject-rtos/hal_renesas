62ac0c6feb59501c094a71e93c4eab2be3e57afb
Thao Luong <thao.luong.uw@renesas.com>
2024-08-29 00:19:05 +0700

    hal: ra: Update Ethernet driver for RA board
    Initial support for Ethernet and Ethernet phy driver
    
    Signed-off-by: Duy Phuong Hoang. Nguyen <duy.nguyen.xa@renesas.com>
    Signed-off-by: Thao Luong <thao.luong.uw@renesas.com>

---
+++ zephyr/ra/ra_cfg/fsp_cfg/r_ether_cfg.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2020 - 2024 Renesas Electronics Corporation and/or its
+ * affiliates
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#ifndef R_ETHER_CFG_H_
+#define R_ETHER_CFG_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define ETHER_CFG_PARAM_CHECKING_ENABLE (BSP_CFG_PARAM_CHECKING_ENABLE)
+#define ETHER_CFG_LINK_PRESENT (0)
+#define ETHER_CFG_USE_LINKSTA (0)
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* R_ETHER_CFG_H_ */
