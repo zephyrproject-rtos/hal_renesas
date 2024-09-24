7a6ef3816b096a56124600bb8b149101be4da3f1
Thao Luong <thao.luong.uw@renesas.com>
2024-09-25 01:43:21 +0700

    hal: renesas: ra: initial support for qspi
    Add fsp support for qspi module
    
    Signed-off-by: Thao Luong <thao.luong.uw@renesas.com>

---
+++ zephyr/ra/ra_cfg/fsp_cfg/r_qspi_cfg.h
@@ -0,0 +1,19 @@
+/*
+* Copyright (c) 2020 - 2024 Renesas Electronics Corporation and/or its affiliates
+*
+* SPDX-License-Identifier: BSD-3-Clause
+*/
+
+#ifndef R_QSPI_CFG_H_
+#define R_QSPI_CFG_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define QSPI_CFG_PARAM_CHECKING_ENABLE (BSP_CFG_PARAM_CHECKING_ENABLE)
+#define QSPI_CFG_SUPPORT_EXTENDED_SPI_MULTI_LINE_PROGRAM  (0)
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* R_QSPI_CFG_H_ */
