ddeab38ff76a277b86b25789f89f3cd73089cff0
The Nguyen <the.nguyen.yf@renesas.com>
2024-11-06 15:35:59 +0700

    hal: renesas : ra: add r_usb_device support on USBFS
    Add support for USBFS on r_usb_device
    
    Signed-off-by: The Nguyen <the.nguyen.yf@renesas.com>

--- drivers/ra/fsp/src/bsp/mcu/ra6m5/bsp_feature.h
+++ drivers/ra/fsp/src/bsp/mcu/ra6m5/bsp_feature.h
@@ -442,4 +442,7 @@
 #define BSP_FEATURE_ULPT_MAX_CHANNEL_NUM                     (0)
 #define BSP_FEATURE_ULPT_VALID_CHANNEL_MASK                  (0U)
 
+#define BSP_FEATURE_USB_NUM_IP                               (2U)
+#define BSP_FEATURE_USB_HAS_USBHS                            (1U)
+
 #endif
