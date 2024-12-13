4dedba1692783246204687ca199c005bf213b68d
Nhut Nguyen <nhut.nguyen.kc@renesas.com>
2024-12-13 10:45:33 +0700

    hal: renesas: rz: Initial support for ADC
    Initial HAL support for ADC
    
    Signed-off-by: Phuc Pham <phuc.pham.xr@bp.renesas.com>
    Signed-off-by: Nhut Nguyen <nhut.nguyen.kc@renesas.com>

---
+++ zephyr/rz/rz_cfg/fsp_cfg/rzg/r_adc_c_cfg.h
@@ -0,0 +1,10 @@
+/*
+* Copyright (c) 2020 - 2024 Renesas Electronics Corporation and/or its affiliates
+*
+* SPDX-License-Identifier: BSD-3-Clause
+*/
+
+#ifndef R_ADC_C_CFG_H_
+#define R_ADC_C_CFG_H_
+#define ADC_C_CFG_PARAM_CHECKING_ENABLE (BSP_CFG_PARAM_CHECKING_ENABLE)
+#endif /* R_ADC_C_CFG_H_ */
