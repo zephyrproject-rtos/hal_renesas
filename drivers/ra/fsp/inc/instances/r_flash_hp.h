8bf596a74a2c42130079361453aa3d20f835b001
Khoa Nguyen <khoa.nguyen.xh@renesas.com>
2024-12-26 16:19:30 +0700

    hal: renesas: ra: modify BSP_PLACE_IN_SECTION to .ramfunc section
    Move the place to store bsp from .code_in_ram section to .ramfunc
    
    Signed-off-by: Khoa Nguyen <khoa.nguyen.xh@renesas.com>

--- drivers/ra/fsp/inc/instances/r_flash_hp.h
+++ drivers/ra/fsp/inc/instances/r_flash_hp.h
@@ -31,12 +31,12 @@ FSP_HEADER
 /* If Code Flash programming is enabled, then all API functions must execute out of RAM. */
 #if (FLASH_HP_CFG_CODE_FLASH_PROGRAMMING_ENABLE == 1)
  #if defined(__ICCARM__)
-  #pragma section=".code_in_ram"
+  #pragma section=".ramfunc"
  #endif
  #if defined(__ARMCC_VERSION) || defined(__GNUC__)
-  #define PLACE_IN_RAM_SECTION    __attribute__((noinline)) BSP_PLACE_IN_SECTION(".code_in_ram")
+  #define PLACE_IN_RAM_SECTION    __attribute__((noinline)) BSP_PLACE_IN_SECTION(".ramfunc")
  #else
-  #define PLACE_IN_RAM_SECTION    BSP_PLACE_IN_SECTION(".code_in_ram")
+  #define PLACE_IN_RAM_SECTION    BSP_PLACE_IN_SECTION(".ramfunc")
  #endif
 #else
  #define PLACE_IN_RAM_SECTION
