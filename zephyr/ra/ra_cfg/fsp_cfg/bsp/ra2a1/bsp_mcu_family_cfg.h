b1793532f34fcc7de7d9315983399ebb502ecdb1
Khoa Nguyen <khoa.nguyen.xh@renesas.com>
2025-02-24 14:10:21 +0700

    hal: renesas: ra: Modify BSP_PRV_IELS_ENUM for build process
    Modify BSP_PRV_IELS_ENUM to use CONCAT instead of ##
    
    Signed-off-by: Khoa Nguyen <khoa.nguyen.xh@renesas.com>

--- zephyr/ra/ra_cfg/fsp_cfg/bsp/ra2a1/bsp_mcu_family_cfg.h
+++ zephyr/ra/ra_cfg/fsp_cfg/bsp/ra2a1/bsp_mcu_family_cfg.h
@@ -72,7 +72,7 @@
 #endif
 
 /* Used to create IELS values for the interrupt initialization table g_interrupt_event_link_select. */
-#define BSP_PRV_IELS_ENUM(vector)    (ELC_ ## vector)
+#define BSP_PRV_IELS_ENUM(vector) CONCAT(ELC_, vector)
 /*
  ID Code
  Note: To permanently lock and disable the debug interface define the BSP_ID_CODE_PERMANENTLY_LOCKED in the compiler settings.
