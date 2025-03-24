d5af372749ecca57966f7f4021571f0fa2fc979c
Tien Nguyen <tien.nguyen.zg@renesas.com>
2025-03-24 09:13:27 +0000

    hal: renesas: Add initial support for RZ/G2L
    Add initial HAL support for Renesas RZ/G2L
    
    Signed-off-by: Tien Nguyen <tien.nguyen.zg@renesas.com>
    Signed-off-by: Nhut Nguyen <nhut.nguyen.kc@renesas.com>

---
+++ drivers/rz/fsp/src/rzg/bsp/cmsis/Device/RENESAS/Include/R9A07G044L/iobitmasks/tsu_iobitmask.h
@@ -0,0 +1,16 @@
+/*
+* Copyright (c) 2020 - 2024 Renesas Electronics Corporation and/or its affiliates
+*
+* SPDX-License-Identifier: BSD-3-Clause
+*/
+
+/**********************************************************************************************************************
+ * File Name    : tsu_iobitmask.h
+ * Version      : 1.00
+ * Description  : IO bit mask file for tsu.
+ *********************************************************************************************************************/
+
+#ifndef TSU_IOBITMASK_H
+#define TSU_IOBITMASK_H
+
+#endif                                 /* TSU_IOBITMASK_H */
