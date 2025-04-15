9e1a16fedeb81f7e5acb93fc41996e93fc97918d
The Nguyen <the.nguyen.yf@renesas.com>
2025-04-15 11:05:32 +0700

    hal: renesas: ra: add support for r_ioport
    First commit to initial support for r_ioport
    
    Signed-off-by: The Nguyen <the.nguyen.yf@renesas.com>

---
+++ zephyr/ra/ra_cfg/fsp_cfg/r_ioport_cfg.h
@@ -0,0 +1,18 @@
+/*
+* Copyright (c) 2020 - 2025 Renesas Electronics Corporation and/or its affiliates
+*
+* SPDX-License-Identifier: BSD-3-Clause
+*/
+
+#ifndef R_IOPORT_CFG_H_
+#define R_IOPORT_CFG_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define IOPORT_CFG_PARAM_CHECKING_ENABLE (BSP_CFG_PARAM_CHECKING_ENABLE)
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* R_IOPORT_CFG_H_ */
