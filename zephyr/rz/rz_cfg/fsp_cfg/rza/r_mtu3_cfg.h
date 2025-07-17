9459cd40ff11ba23049ca63d476cf9b9f81b8f92
Hieu Nguyen <hieu.nguyen.ym@bp.renesas.com>
2025-07-17 06:36:44 +0000

    hal: renesas: rza: Initial PWM support for RZ/A series
    Add MTU FSP HAL driver to support PWM driver for RZ/A series
    
    Signed-off-by: Hieu Nguyen <hieu.nguyen.ym@bp.renesas.com>
    Signed-off-by: Nhut Nguyen <nhut.nguyen.kc@renesas.com>

---
+++ zephyr/rz/rz_cfg/fsp_cfg/rza/r_mtu3_cfg.h
@@ -0,0 +1,19 @@
+/*
+* Copyright (c) 2020 - 2024 Renesas Electronics Corporation and/or its affiliates
+*
+* SPDX-License-Identifier: BSD-3-Clause
+*/
+
+#ifndef R_MTU3_CFG_H_
+#define R_MTU3_CFG_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define MTU3_CFG_PARAM_CHECKING_ENABLE (BSP_CFG_PARAM_CHECKING_ENABLE)
+#define MTU3_CFG_OUTPUT_SUPPORT_ENABLE (1)
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* R_MTU3_CFG_H_ */
