843bd24d911f3a9bce29ee0fdf0a265e87e55016
Phuc Pham <phuc.pham.xr@bp.renesas.com>
2025-07-22 07:04:21 +0000

    hal: renesas: rza: Add I2C support for RZ/A series
    Add HAL FSP I2C files for RZ/A series
    
    Signed-off-by: Phuc Pham <phuc.pham.xr@bp.renesas.com>
    Signed-off-by: Nhut Nguyen <nhut.nguyen.kc@renesas.com>

---
+++ zephyr/rz/rz_cfg/fsp_cfg/rza/r_riic_master_cfg.h
@@ -0,0 +1,20 @@
+/*
+* Copyright (c) 2020 - 2025 Renesas Electronics Corporation and/or its affiliates
+*
+* SPDX-License-Identifier: BSD-3-Clause
+*/
+
+#ifndef R_RIIC_MASTER_CFG_H_
+#define R_RIIC_MASTER_CFG_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define RIIC_MASTER_CFG_PARAM_CHECKING_ENABLE (BSP_CFG_PARAM_CHECKING_ENABLE)
+#define RIIC_MASTER_CFG_DMAC_ENABLE (0)
+#define RIIC_MASTER_CFG_ADDR_MODE_10_BIT_ENABLE (1)
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* R_RIIC_MASTER_CFG_H_ */
