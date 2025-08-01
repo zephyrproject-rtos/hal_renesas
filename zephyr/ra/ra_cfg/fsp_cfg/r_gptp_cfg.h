02ab9794ae266f0714723e1e57ce65ea838b329a
Ta Minh Nhat <nhat-minh.ta.yn@bp.renesas.com>
2025-08-01 14:53:08 +0700

    hal: renesas: ra: add new ethernet driver rmac
    Add new ethernet driver r_rmac, r_rmac_phy, r_layer3_switch,
    r_gptp support.
    
    Signed-off-by: Nhat Ta <nhat-minh.ta.yn@bp.renesas.com>

---
+++ zephyr/ra/ra_cfg/fsp_cfg/r_gptp_cfg.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2025 Renesas Electronics Corporation and/or its affiliates
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef R_GPTP_CFG_H_
+#define R_GPTP_CFG_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define GPTP_CFG_PARAM_CHECKING_ENABLE (BSP_CFG_PARAM_CHECKING_ENABLE)
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* R_GPTP_CFG_H_ */
