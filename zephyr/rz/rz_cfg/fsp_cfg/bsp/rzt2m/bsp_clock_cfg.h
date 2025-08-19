1104a870749840df610f70bf8d1ebe3a36f9d137
Nhut Nguyen <nhut.nguyen.kc@renesas.com>
2025-08-19 14:47:30 +0700

    hal: renesas: rzt: Add clock control support for RZ/T2M
    Add clock control support for RZ/T2M.
    Clock frequency and clock settings are taken from dts.
    
    Signed-off-by: Nhut Nguyen <nhut.nguyen.kc@renesas.com>
    Signed-off-by: Quang Le <quang.le.eb@bp.renesas.com>

--- zephyr/rz/rz_cfg/fsp_cfg/bsp/rzt2m/bsp_clock_cfg.h
+++ zephyr/rz/rz_cfg/fsp_cfg/bsp/rzt2m/bsp_clock_cfg.h
@@ -4,45 +4,93 @@
 * SPDX-License-Identifier: BSD-3-Clause
 */
 
+#include <zephyr/drivers/clock_control/renesas_rz_cgc.h>
+
 #ifndef BSP_CLOCK_CFG_H_
 #define BSP_CLOCK_CFG_H_
 #define BSP_CFG_CLOCKS_SECURE (0)
 #define BSP_CFG_CLOCKS_OVERRIDE (0)
-#define BSP_CFG_MAIN_CLOCK_HZ (25000000) /* Main Clock: 25MHz */
-#define BSP_CFG_LOCO_ENABLE (BSP_CLOCKS_LOCO_ENABLE) /* LOCO Enabled */
-#define BSP_CFG_PLL1 (BSP_CLOCKS_PLL1_INITIAL) /* PLL1 is initial state */
-#define BSP_CFG_PHYSEL (BSP_CLOCKS_PHYSEL_PLL1_DIV) /* Ethernet Clock src: PLL1 divider clock */
-#define BSP_CFG_CLMA0_ENABLE (BSP_CLOCKS_CLMA0_ENABLE) /* CLMA0 Enabled */
-#define BSP_CFG_CLMA0MASK (BSP_CLOCKS_CLMA0_ERROR_NOT_MASK) /* CLMA0 error not mask */
-#define BSP_CFG_CLMA3MASK (BSP_CLOCKS_CLMA3_ERROR_NOT_MASK) /* CLMA3 error not mask */
-#define BSP_CFG_CLMA1MASK (BSP_CLOCKS_CLMA1_ERROR_MASK) /* CLMA1 error mask */
-#define BSP_CFG_CLMA3_ENABLE (BSP_CLOCKS_CLMA3_ENABLE) /* CLMA3 Enabled */
-#define BSP_CFG_CLMA1_ENABLE (BSP_CLOCKS_CLMA1_ENABLE) /* CLMA1 Enabled */
-#define BSP_CFG_CLMA2_ENABLE (BSP_CLOCKS_CLMA2_ENABLE) /* CLMA2 Enabled */
-#define BSP_CFG_CLMA0_CMPL (1) /* CLMA0 CMPL 1 */
-#define BSP_CFG_CLMA1_CMPL (1) /* CLMA1 CMPL 1 */
-#define BSP_CFG_CLMA2_CMPL (1) /* CLMA2 CMPL 1 */
-#define BSP_CFG_CLMA3_CMPL (1) /* CLMA3 CMPL 1 */
-#define BSP_CFG_CLMASEL (BSP_CLOCKS_CLMASEL_LOCO) /* Alternative clock: LOCO */
-#define BSP_CFG_CLMA0_CMPH (1023) /* CLMA0 CMPH 1023 */
-#define BSP_CFG_CLMA1_CMPH (1023) /* CLMA1 CMPH 1023 */
-#define BSP_CFG_CLMA2_CMPH (1023) /* CLMA2 CMPH 1023 */
-#define BSP_CFG_CLMA3_CMPH (1023) /* CLMA3 CMPH 1023 */
-#define BSP_CFG_DIVSELSUB (BSP_CLOCKS_DIVSELSUB_0) /* ICLK 200MHz */
-#define BSP_CFG_SCI0ASYNCCLK (BSP_CLOCKS_SCI0_ASYNCHRONOUS_SERIAL_CLOCK_96_MHZ) /* SCI0ASYNCCLK: 96MHz */
-#define BSP_CFG_SCI1ASYNCCLK (BSP_CLOCKS_SCI1_ASYNCHRONOUS_SERIAL_CLOCK_96_MHZ) /* SCI1ASYNCCLK: 96MHz */
-#define BSP_CFG_SCI2ASYNCCLK (BSP_CLOCKS_SCI2_ASYNCHRONOUS_SERIAL_CLOCK_96_MHZ) /* SCI2ASYNCCLK: 96MHz */
-#define BSP_CFG_SCI3ASYNCCLK (BSP_CLOCKS_SCI3_ASYNCHRONOUS_SERIAL_CLOCK_96_MHZ) /* SCI3ASYNCCLK: 96MHz */
-#define BSP_CFG_SCI4ASYNCCLK (BSP_CLOCKS_SCI4_ASYNCHRONOUS_SERIAL_CLOCK_96_MHZ) /* SCI4ASYNCCLK: 96MHz */
-#define BSP_CFG_SCI5ASYNCCLK (BSP_CLOCKS_SCI5_ASYNCHRONOUS_SERIAL_CLOCK_96_MHZ) /* SCI5ASYNCCLK: 96MHz */
-#define BSP_CFG_SPI0ASYNCCLK (BSP_CLOCKS_SPI0_ASYNCHRONOUS_SERIAL_CLOCK_96_MHZ) /* SPI0ASYNCCLK: 96MHz */
-#define BSP_CFG_SPI1ASYNCCLK (BSP_CLOCKS_SPI1_ASYNCHRONOUS_SERIAL_CLOCK_96_MHZ) /* SPI1ASYNCCLK: 96MHz */
-#define BSP_CFG_SPI2ASYNCCLK (BSP_CLOCKS_SPI2_ASYNCHRONOUS_SERIAL_CLOCK_96_MHZ) /* SPI2ASYNCCLK: 96MHz */
-#define BSP_CFG_SPI3ASYNCCLK (BSP_CLOCKS_SPI3_ASYNCHRONOUS_SERIAL_CLOCK_96_MHZ) /* SPI3ASYNCCLK: 96MHz */
-#define BSP_CFG_FSELCPU0 (BSP_CLOCKS_FSELCPU0_ICLK_MUL1) /* CPU0CLK Mul x1 */
-#define BSP_CFG_FSELCPU1 (BSP_CLOCKS_FSELCPU1_ICLK_MUL1) /* CPU1CLK Mul x1 */
-#define BSP_CFG_CKIO (BSP_CLOCKS_CKIO_ICLK_DIV4) /* CKIO Div /4 */
-#define BSP_CFG_FSELCANFD (BSP_CLOCKS_CANFD_CLOCK_40_MHZ) /* CANFDCLK Src: PCLKCAN 40MHz */
-#define BSP_CFG_FSELXSPI0_DIVSELXSPI0 (BSP_CLOCKS_XSPI0_CLOCK_DIV0_12_5_MHZ) /* XSPI_CLK0 12.5MHz */
-#define BSP_CFG_FSELXSPI1_DIVSELXSPI1 (BSP_CLOCKS_XSPI1_CLOCK_DIV0_12_5_MHZ) /* XSPI_CLK1 12.5MHz */
+#define BSP_CFG_MAIN_CLOCK_HZ (DT_PROP(DT_NODELABEL(osc), clock_frequency)) /* Main Clock: 25MHz */
+BUILD_ASSERT(BSP_CFG_MAIN_CLOCK_HZ == 25000000, "The main clock must be 25MHz!");
+
+/* LOCO Enable/Disable */
+#define BSP_CFG_LOCO_ENABLE                                                                        \
+	((DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(loco)) == 1) ? BSP_CLOCKS_LOCO_ENABLE               \
+							    : BSP_CLOCKS_LOCO_DISABLE)
+
+/* PLL1 initial state */
+#define BSP_CFG_PLL1                                                                               \
+	((DT_ENUM_IDX(DT_NODELABEL(pll1), state) == 0)  ? BSP_CLOCKS_PLL1_INITIAL                  \
+	: (DT_ENUM_IDX(DT_NODELABEL(pll1), state) == 1) ? BSP_CLOCKS_PLL1_STANDBY                  \
+							: BSP_CLOCKS_PLL1_NORMAL)
+
+/* Ethernet Clock src */
+#define BSP_CFG_PHYSEL	DT_ENUM_IDX(DT_NODELABEL(eth_refclk), eth_phy_source)
+
+#define BSP_CFG_CLMA0_ENABLE 	(BSP_CLOCKS_CLMA0_ENABLE) 	  /* CLMA0 Enabled */
+#define BSP_CFG_CLMA0MASK 	(BSP_CLOCKS_CLMA0_ERROR_NOT_MASK) /* CLMA0 error not mask */
+#define BSP_CFG_CLMA3MASK 	(BSP_CLOCKS_CLMA3_ERROR_NOT_MASK) /* CLMA3 error not mask */
+#define BSP_CFG_CLMA1MASK 	(BSP_CLOCKS_CLMA1_ERROR_MASK) 	  /* CLMA1 error mask */
+#define BSP_CFG_CLMA3_ENABLE 	(BSP_CLOCKS_CLMA3_ENABLE) 	  /* CLMA3 Enabled */
+#define BSP_CFG_CLMA1_ENABLE 	(BSP_CLOCKS_CLMA1_ENABLE) 	  /* CLMA1 Enabled */
+#define BSP_CFG_CLMA2_ENABLE 	(BSP_CLOCKS_CLMA2_ENABLE) 	  /* CLMA2 Enabled */
+#define BSP_CFG_CLMA0_CMPL 	(1) 				  /* CLMA0 CMPL 1 */
+#define BSP_CFG_CLMA1_CMPL 	(1) 				  /* CLMA1 CMPL 1 */
+#define BSP_CFG_CLMA2_CMPL 	(1) 				  /* CLMA2 CMPL 1 */
+#define BSP_CFG_CLMA3_CMPL 	(1) 				  /* CLMA3 CMPL 1 */
+#define BSP_CFG_CLMASEL 	(BSP_CLOCKS_CLMASEL_LOCO) 	  /* Alternative clock: LOCO */
+#define BSP_CFG_CLMA0_CMPH 	(1023) 				  /* CLMA0 CMPH 1023 */
+#define BSP_CFG_CLMA1_CMPH 	(1023) 				  /* CLMA1 CMPH 1023 */
+#define BSP_CFG_CLMA2_CMPH 	(1023) 				  /* CLMA2 CMPH 1023 */
+#define BSP_CFG_CLMA3_CMPH 	(1023) 				  /* CLMA3 CMPH 1023 */
+
+#define BSP_CFG_DIVSELSUB	DT_ENUM_IDX(DT_NODELABEL(iclk), clock_frequency)	 /* ICLK */
+#define BSP_CFG_SCI0ASYNCCLK	DT_ENUM_IDX(DT_NODELABEL(sci0asyncclk), clock_frequency) /* SCI0ASYNCCLK */
+#define BSP_CFG_SCI1ASYNCCLK	DT_ENUM_IDX(DT_NODELABEL(sci1asyncclk), clock_frequency) /* SCI1ASYNCCLK */
+#define BSP_CFG_SCI2ASYNCCLK	DT_ENUM_IDX(DT_NODELABEL(sci2asyncclk), clock_frequency) /* SCI2ASYNCCLK */
+#define BSP_CFG_SCI3ASYNCCLK	DT_ENUM_IDX(DT_NODELABEL(sci3asyncclk), clock_frequency) /* SCI3ASYNCCLK */
+#define BSP_CFG_SCI4ASYNCCLK	DT_ENUM_IDX(DT_NODELABEL(sci4asyncclk), clock_frequency) /* SCI4ASYNCCLK */
+#define BSP_CFG_SCI5ASYNCCLK	DT_ENUM_IDX(DT_NODELABEL(sci5asyncclk), clock_frequency) /* SCI5ASYNCCLK */
+#define BSP_CFG_SPI0ASYNCCLK	DT_ENUM_IDX(DT_NODELABEL(spi0asyncclk), clock_frequency) /* SPI0ASYNCCLK */
+#define BSP_CFG_SPI1ASYNCCLK	DT_ENUM_IDX(DT_NODELABEL(spi1asyncclk), clock_frequency) /* SPI1ASYNCCLK */
+#define BSP_CFG_SPI2ASYNCCLK	DT_ENUM_IDX(DT_NODELABEL(spi2asyncclk), clock_frequency) /* SPI2ASYNCCLK */
+#define BSP_CFG_SPI3ASYNCCLK	DT_ENUM_IDX(DT_NODELABEL(spi3asyncclk), clock_frequency) /* SPI3ASYNCCLK */
+#define BSP_CFG_FSELCPU0 	RZ_CGC_SUBCLK_MUL(DT_NODELABEL(cpu0clk)) 		 /* CPU0CLK Mul */
+#define BSP_CFG_FSELCPU1 	RZ_CGC_SUBCLK_MUL(DT_NODELABEL(cpu1clk)) 		 /* CPU1CLK Mul */
+#define BSP_CFG_CKIO 		RZ_CGC_SUBCLK_DIV(DT_NODELABEL(ckio))			 /* CKIO Div */
+
+/* Validate the CPU0 multiplier */
+BUILD_ASSERT(IN_RANGE(LOG2(DT_PROP(DT_NODELABEL(cpu0clk), mul)), 0, 2) == 1,
+			"The CPU0 multiplier must be 1, 2, or 4!");
+
+/* Validate the CPU1 multiplier */
+BUILD_ASSERT(IN_RANGE(LOG2(DT_PROP(DT_NODELABEL(cpu1clk), mul)), 0, 2) == 1,
+			"The CPU1 multiplier must be 1, 2, or 4!");
+
+/* CANFDCLK Src */
+#define BSP_CFG_FSELCANFD                                                                          \
+	((DT_ENUM_IDX(DT_NODELABEL(canfdclk), canfd_source) == 0) ? BSP_CLOCKS_CANFD_CLOCK_80_MHZ  \
+	: (DT_ENUM_IDX(DT_NODELABEL(canfdclk), canfd_source) == 1) ? BSP_CLOCKS_CANFD_CLOCK_40_MHZ \
+								   : BSP_CLOCKS_CANFD_CLOCK_PCLKM)
+
+/* XSPI_CLK0 */
+#define BSP_CFG_FSELXSPI0_DIVSELXSPI0                                                                                 \
+	((DT_PROP(DT_NODELABEL(xspi_clk0), xspi_clk_frequency) == 133333333)  ? BSP_CLOCKS_XSPI0_CLOCK_DIV0_133_3_MHZ \
+	: (DT_PROP(DT_NODELABEL(xspi_clk0), xspi_clk_frequency) == 100000000) ? BSP_CLOCKS_XSPI0_CLOCK_DIV0_100_0_MHZ \
+	: (DT_PROP(DT_NODELABEL(xspi_clk0), xspi_clk_frequency) == 50000000)  ? BSP_CLOCKS_XSPI0_CLOCK_DIV0_50_0_MHZ  \
+	: (DT_PROP(DT_NODELABEL(xspi_clk0), xspi_clk_frequency) == 25000000)  ? BSP_CLOCKS_XSPI0_CLOCK_DIV0_25_0_MHZ  \
+	: (DT_PROP(DT_NODELABEL(xspi_clk0), xspi_clk_frequency) == 12500000)  ? BSP_CLOCKS_XSPI0_CLOCK_DIV0_12_5_MHZ  \
+	: (DT_PROP(DT_NODELABEL(xspi_clk0), xspi_clk_frequency) == 75000000)  ? BSP_CLOCKS_XSPI0_CLOCK_DIV1_75_0_MHZ  \
+									      : BSP_CLOCKS_XSPI0_CLOCK_DIV1_37_5_MHZ)
+
+/* XSPI_CLK1 */
+#define BSP_CFG_FSELXSPI1_DIVSELXSPI1                                                                                 \
+	((DT_PROP(DT_NODELABEL(xspi_clk1), xspi_clk_frequency) == 133333333)  ? BSP_CLOCKS_XSPI1_CLOCK_DIV0_133_3_MHZ \
+	: (DT_PROP(DT_NODELABEL(xspi_clk1), xspi_clk_frequency) == 100000000) ? BSP_CLOCKS_XSPI1_CLOCK_DIV0_100_0_MHZ \
+	: (DT_PROP(DT_NODELABEL(xspi_clk1), xspi_clk_frequency) == 50000000)  ? BSP_CLOCKS_XSPI1_CLOCK_DIV0_50_0_MHZ  \
+	: (DT_PROP(DT_NODELABEL(xspi_clk1), xspi_clk_frequency) == 25000000)  ? BSP_CLOCKS_XSPI1_CLOCK_DIV0_25_0_MHZ  \
+	: (DT_PROP(DT_NODELABEL(xspi_clk1), xspi_clk_frequency) == 12500000)  ? BSP_CLOCKS_XSPI1_CLOCK_DIV0_12_5_MHZ  \
+	: (DT_PROP(DT_NODELABEL(xspi_clk1), xspi_clk_frequency) == 75000000)  ? BSP_CLOCKS_XSPI1_CLOCK_DIV1_75_0_MHZ  \
+									      : BSP_CLOCKS_XSPI1_CLOCK_DIV1_37_5_MHZ)
+
 #endif /* BSP_CLOCK_CFG_H_ */
