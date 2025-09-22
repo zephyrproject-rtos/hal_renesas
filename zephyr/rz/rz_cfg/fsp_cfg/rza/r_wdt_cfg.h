2c98e5dd0a1529438a118c59f705c286060d4239
Nhut Nguyen <nhut.nguyen.kc@renesas.com>
2025-09-22 12:54:53 +0700

    hal: renesas: rza: Add WDT support for RZ/A series
    Add WDT FSP HAL driver to support Zephyr WDT driver for RZ/A3UL
    
    Signed-off-by: Nhut Nguyen <nhut.nguyen.kc@renesas.com>
    Signed-off-by: Quang Le <quang.le.eb@bp.renesas.com>

---
+++ zephyr/rz/rz_cfg/fsp_cfg/rza/r_wdt_cfg.h
@@ -0,0 +1,18 @@
+/*
+* Copyright (c) 2020 - 2025 Renesas Electronics Corporation and/or its affiliates
+*
+* SPDX-License-Identifier: BSD-3-Clause
+*/
+
+#ifndef R_WDT_CFG_H_
+#define R_WDT_CFG_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define WDT_CFG_PARAM_CHECKING_ENABLE BSP_CFG_PARAM_CHECKING_ENABLE
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* R_WDT_CFG_H_ */
