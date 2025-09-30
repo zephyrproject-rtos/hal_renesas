46918b5f7cd4bcbe313091d369f9d7fcd4390eab
Tien Nguyen <tien.nguyen.zg@renesas.com>
2025-09-30 09:20:07 +0700

    hal: renesas: Add Flash support for RZ/A series
    Add SPIBSC FSP HAL driver to support Zephyr Flash driver for RZ/A3UL
    
    Signed-off-by: Tien Nguyen <tien.nguyen.zg@renesas.com>

---
+++ zephyr/rz/rz_cfg/fsp_cfg/rza/r_spibsc_cfg.h
@@ -0,0 +1,13 @@
+/*
+* Copyright (c) 2020 - 2025 Renesas Electronics Corporation and/or its affiliates
+*
+* SPDX-License-Identifier: BSD-3-Clause
+*/
+
+#ifndef R_SPIBSC_CFG_H_
+#define R_SPIBSC_CFG_H_
+#define SPIBSC_CFG_PARAM_CHECKING_ENABLE (BSP_CFG_PARAM_CHECKING_ENABLE)
+#define SPIBSC_CFG_INITIALIZATION (0)
+#define SPIBSC_CFG_CODE_SECTION ".text"
+#define SPIBSC_CFG_MMAP_BASE 0x20000000
+#endif /* R_SPIBSC_CFG_H_ */
