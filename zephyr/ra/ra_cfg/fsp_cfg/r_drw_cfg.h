de1ad0ecebf91af8f42cf68687eccbd9270df56c
Duy Nguyen <duy.nguyen.xa@renesas.com>
2025-10-08 03:06:30 +0000

    hal: ra: Initial update for drw driver
    This is the commit to support for drw driver on Renesas RA
    The commit add dave2d lib as static blob library and the drw
    driver code in FSP
    
    Signed-off-by: Duy Nguyen <duy.nguyen.xa@renesas.com>

---
+++ zephyr/ra/ra_cfg/fsp_cfg/r_drw_cfg.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2020 - 2025 Renesas Electronics Corporation and/or its affiliates
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef R_DRW_CFG_H_
+#define R_DRW_CFG_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define DRW_CFG_USE_DLIST_INDIRECT ((1))
+#define DRW_CFG_CUSTOM_MALLOC      ((1))
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* R_DRW_CFG_H_ */
--- zephyr/ra/ra_gen/hal_data.h
+++ zephyr/ra/ra_gen/hal_data.h
@@ -7,14 +7,30 @@
 #ifndef HAL_DATA_H_
 #define HAL_DATA_H_
 #include "bsp_api.h"
+
+#if CONFIG_INPUT_RENESAS_RA_QE_TOUCH_CFG
 #include "rm_touch.h"
+#endif
+
+#if CONFIG_LV_USE_DRAW_DAVE2D
+#include "dave_base.h"
+#include "dave_driver.h"
+#include "dave_math.h"
+#include "dave_types.h"
+#include "r_glcdc.h"
+#include <zephyr/devicetree.h>
+#include <zephyr/dt-bindings/display/panel.h>
+#endif
+
 FSP_HEADER
 
+#if CONFIG_INPUT_RENESAS_RA_QE_TOUCH_CFG
 #define CTSU_WRITE_IRQn                                                        \
   (DT_IRQ_BY_NAME(DT_INST(0, renesas_ra_ctsu), ctsuwr, irq))
 #define CTSU_READ_IRQn                                                         \
   (DT_IRQ_BY_NAME(DT_INST(0, renesas_ra_ctsu), ctsurd, irq))
 #define CTSU_END_IRQn (DT_IRQ_BY_NAME(DT_INST(0, renesas_ra_ctsu), ctsufn, irq))
+#endif
 
 FSP_FOOTER
 #endif /* HAL_DATA_H_ */
