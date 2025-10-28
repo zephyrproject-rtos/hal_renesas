512f2299241031a40ad29959ae030340abc01ede
Nhat Ta <nhat-minh.ta.yn@bp.renesas.com>
2025-10-28 13:20:52 +0700

    hal: ra: Add macros for available queue num
    This commit adds macros to calculate available queue num from devicetree.
    
    Signed-off-by: Nhat Ta <nhat-minh.ta.yn@bp.renesas.com>

--- zephyr/ra/ra_cfg/fsp_cfg/r_layer3_switch_cfg.h
+++ zephyr/ra/ra_cfg/fsp_cfg/r_layer3_switch_cfg.h
@@ -12,6 +12,15 @@ extern "C" {
 
 #define LAYER3_SWITCH_CFG_PARAM_CHECKING_ENABLE (BSP_CFG_PARAM_CHECKING_ENABLE)
 
+#define LAYER3_SWITCH_GET_QUEUE_NUM(id, compat, prop)                                  \
+		    COND_CODE_1(DT_NODE_HAS_COMPAT(id,compat),(DT_PROP(id,prop)),(0))
+
+#define LAYER3_SWITCH_CFG_AVAILABLE_QUEUE_NUM                                          \
+		    (DT_FOREACH_CHILD_SEP_VARGS(DT_NODELABEL(eswm), LAYER3_SWITCH_GET_QUEUE_NUM,   \
+						(+), renesas_ra_ethernet_rmac, txq_num) +          \
+		     DT_FOREACH_CHILD_SEP_VARGS(DT_NODELABEL(eswm), LAYER3_SWITCH_GET_QUEUE_NUM,   \
+						(+), renesas_ra_ethernet_rmac, rxq_num))
+
 #ifdef __cplusplus
 }
 #endif
