c2763b484e3a2901eea49a0f78cbafcc3f37480d
Vincent Jardin <vjardin@free.fr>
2025-12-15 02:35:40 +0100

    hal: renesas: ra: add Zephyr integration for I2C slave drivers
    Add configuration headers and CMake rules for building the IIC, IIC_B,
    and IICA slave drivers with Zephyr.
    
    Configuration headers provide the necessary declarations and macros
    expected by the FSP driver sources. CMakeLists.txt is updated with
    conditional compilation rules using the following Kconfig options:
    - CONFIG_USE_RA_FSP_IIC_SLAVE
    - CONFIG_USE_RA_FSP_IIC_B_SLAVE
    - CONFIG_USE_RA_FSP_IICA_SLAVE
    
    Signed-off-by: Vincent Jardin <vjardin@free.fr>

---
+++ zephyr/ra/ra_cfg/fsp_cfg/r_iic_slave_cfg.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2025 Vincent Jardin, Free Mobile
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef R_IIC_SLAVE_CFG_H
+#define R_IIC_SLAVE_CFG_H
+
+#include "bsp_api.h"
+#include "r_i2c_slave_api.h"
+
+/* Common macro for FSP header files. There is also a corresponding FSP_FOOTER macro at the end of this file. */
+FSP_HEADER
+
+/** Specifies whether to include code for DTC support */
+#define IIC_SLAVE_CFG_DTC_ENABLE (0)
+
+/** Filled in Interface API structure for this Instance. */
+extern i2c_slave_api_t const g_i2c_slave_on_iic;
+
+/** Common macro for FSP header files. */
+FSP_FOOTER
+
+#endif /* R_IIC_SLAVE_CFG_H */
