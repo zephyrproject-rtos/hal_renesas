25fa688249ffaa6e35ff32590b5fdb6f6ae80377
Thinh Le Cong <thinh.le.xr@bp.renesas.com>
2025-12-22 16:10:21 +0700

    hal: renesas: ra: Add HAL support for Renesas RA8E1 SoC series
    Add HAL support for Renesas RA8E1 SoC Series
    
    Signed-off-by: Thinh Le Cong <thinh.le.xr@bp.renesas.com>

---
+++ zephyr/ra/ra_cfg/fsp_cfg/bsp/ra8e1/bsp_mcu_device_pn_cfg.h
@@ -0,0 +1,23 @@
+/*
+* Copyright (c) 2020 - 2025 Renesas Electronics Corporation and/or its affiliates
+*
+* SPDX-License-Identifier: BSD-3-Clause
+*/
+#ifndef BSP_MCU_DEVICE_PN_CFG_H_
+#define BSP_MCU_DEVICE_PN_CFG_H_
+
+#define BSP_MCU_FEATURE_SET ('A')
+#define BSP_ROM_SIZE_BYTES DT_REG_SIZE(DT_NODELABEL(flash0))
+#define BSP_RAM_SIZE_BYTES (524288)
+#define BSP_DATA_FLASH_SIZE_BYTES DT_REG_SIZE(DT_NODELABEL(flash1))
+#define BSP_NUMBER_OF_CORES (1)
+
+#if (CONFIG_RENESAS_PN_PACKAGE_TYPE == 1)
+#define BSP_PACKAGE_LQFP
+#define BSP_PACKAGE_PINS (144)
+#elif (CONFIG_RENESAS_PN_PACKAGE_TYPE == 2)
+#define BSP_PACKAGE_LQFP
+#define BSP_PACKAGE_PINS (100)
+#endif
+
+#endif /* BSP_MCU_DEVICE_PN_CFG_H_ */
