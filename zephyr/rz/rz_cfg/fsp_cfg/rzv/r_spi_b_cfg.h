be50af06931a1ba56a92c0578760e825d42f9298
Quang Le <quang.le.eb@bp.renesas.com>
2025-12-26 09:24:36 +0000

    hal: renesas: rz: Add SPI support for RZ/V2H, RZ/V2N
    Add HAL FSP SPI files to support SPI for RZ/V2H, RZ/V2N
    
    Signed-off-by: Quang Le <quang.le.eb@bp.renesas.com>
    Signed-off-by: Nhut Nguyen <nhut.nguyen.kc@renesas.com>

---
+++ zephyr/rz/rz_cfg/fsp_cfg/rzv/r_spi_b_cfg.h
@@ -0,0 +1,12 @@
+/*
+* Copyright (c) 2020 - 2024 Renesas Electronics Corporation and/or its affiliates
+*
+* SPDX-License-Identifier: BSD-3-Clause
+*/
+
+#ifndef R_SPI_B_CFG_H_
+#define R_SPI_B_CFG_H_
+#define SPI_B_CFG_PARAM_CHECKING_ENABLE (BSP_CFG_PARAM_CHECKING_ENABLE)
+#define SPI_B_DMAC_SUPPORT_ENABLE CONFIG_SPI_RENESAS_RZ_DMA
+#define SPI_B_TRANSMIT_FROM_RXI_ISR (0)
+#endif /* R_SPI_B_CFG_H_ */
